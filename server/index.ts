8c447f48e4dace592129d7d9eb5ac4e080ea1d34
evmaperry <evmaperry@gmail.com>
2024-03-18 10:12:43 -0400

    SETUP / add webpack, ts config

---
+++ server/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+
+const app = express()
+const port = 3000
+
+const distPath = path.resolve(__dirname, '..', 'dist');
+
+app.use(express.static(distPath));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
